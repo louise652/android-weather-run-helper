d5349348cb84b0e886021ff06debacb934e13c11
Louise McCloy <louise.mccloy@liberty-it.co.uk>
2020-09-01 13:28:58 +0100

    Adding in preliminary gherkins for TDD and ResultsActivity

--- app/src/main/java/com/android/runweather/activities/ResultActivity.java
+++ app/src/main/java/com/android/runweather/activities/ResultActivity.java
@@ -6,12 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.runweather.R;
 
-public class MainActivity extends AppCompatActivity {
+public class ResultActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
+        setContentView(R.layout.activity_result);
     }
 }
--- app/src/test/java/com/android/runweather/activities/MainActivityTest.java
+++ app/src/test/java/com/android/runweather/activities/MainActivityTest.java
@@ -2,8 +2,58 @@ package com.android.runweather.activities;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class MainActivityTest {
+
+    @Test
+    void test_mainComponentsRender(){
+        //Given app is opened
+        //When page loads
+        //Then button renders
+    }
+
+
+    @Test
+    void test_locationPrompt(){
+        //Given app is loaded
+        //When button clicked and location permission is not approved
+        //Then location prompt appears
+    }
+
+
+    @Test
+    void test_locationApproved(){
+        //Given button is clicked
+        //When app asks for location permission and user agrees
+        //Then result activity launches
+    }
+
+
+    @Test
+    void test_locationDenied(){
+        //Given button is clicked
+        //When app asks for location permission and user doesn't agree
+        //Then page loads with manual option to type location
+    }
+
+    @Test
+    void test_manualLocationValid(){
+        //Given button is clicked and user doesn't agree to location permission
+        //When the user enters in a valid manual location
+        //Then result activity launches
+    }
+
+    @Test
+    void test_manualLocationInvalid(){
+        //Given button is clicked and user doesn't agree to location permission
+        //When the user enters in a invalid manual location
+        //An error toast appears
+    }
+
+    @Test
+    void test_help(){
+        //Given app is loaded
+        //When button help icon is clicked
+        //Then help text appears
+    }
 
 }
