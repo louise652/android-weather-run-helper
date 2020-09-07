3019ec6bf6d2861f61ff6e698caef4deacdfebd5
louise652 <lmccloy652@gmail.com>
2020-09-07 16:26:41 +0100

    Fixing places autocomplete for manual location

--- app/src/main/java/com/android/runweather/activities/MainActivity.java
+++ app/src/main/java/com/android/runweather/activities/MainActivity.java
@@ -1,6 +1,7 @@
 package com.android.runweather.activities;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,42 +19,47 @@ import com.google.android.libraries.places.widget.listener.PlaceSelectionListene
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-    TextView txtView;
+    TextView txtCity;
     String locationServiceString;
+    View view;
+    AutocompleteSupportFragment autocompleteFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        autocompleteFragment = initManualPlaceSelection();
+
+        view = findViewById(R.id.manual_container);
         locationServiceString = LocationUtil.getInstance(this).checkLocationPermission();
         System.out.println(locationServiceString);
-        Toast.makeText(this, locationServiceString, Toast.LENGTH_LONG).show();
 
-        if (locationServiceString.isEmpty()) {
-            getManualLocation();
+        if (!locationServiceString.isEmpty()) {
+            autocompleteFragment.setText(locationServiceString);
+
         }
+
+        initManualLocation();
 
     }
 
-
-    private void getManualLocation() {
-        // Initialize Places.
-        Places.initialize(getApplicationContext(), "YOUR_API_KEY");
-        // Create a new Places client instance.
-        PlacesClient placesClient = Places.createClient(this);
-
-        // Initialize the AutocompleteSupportFragment.
-        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
-                getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+    /**
+     * Overidden methods to handle manual location entry
+     */
+    private void initManualLocation() {
 
         // Specify the types of place data to return.
         autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME));
+
+        autocompleteFragment.setHint("Enter your location");
 
         // Set up a PlaceSelectionListener to handle the response.
         autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
             public void onPlaceSelected(Place place) {
-                // TODO: Get info about the selected place.
-                txtView.setText(place.getName() + "," + place.getId());
+
+                locationServiceString = place.getName();
                 System.out.println("Place: " + place.getName() + ", " + place.getId());
             }
 
@@ -66,4 +72,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    /**
+     * Setup the AutoCompleteSupport fragment. This allows the user to serach for a location if they do not want to use the lcoation services
+     *
+     * @return
+     */
+    private AutocompleteSupportFragment initManualPlaceSelection() {
+        // Initialize Places.
+        Places.initialize(getApplicationContext(), "AIzaSyD9Qbv5Ve6hw76OEHA9GyGyUMQP0lD3uVE");
+        // Create a new Places client instance.
+        PlacesClient placesClient = Places.createClient(this);
+
+        // Initialize the AutocompleteSupportFragment.
+        return (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+    }
+
+    /**
+     * Call out to weather API with the location passed as a param on button click
+     *
+     * @param view
+     */
+    public void callWeather(View view) {
+        Toast.makeText(this, "Calling API with location: " + locationServiceString, Toast.LENGTH_LONG).show();
+    }
 }
