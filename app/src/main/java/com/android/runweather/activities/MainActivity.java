c23a39fc1d3781fdbf335342d8283342300c4764
louise652 <lmccloy652@gmail.com>
2020-09-07 11:30:59 +0100

    First steps to use google places autocomplete for manual location entry

--- app/src/main/java/com/android/runweather/activities/MainActivity.java
+++ app/src/main/java/com/android/runweather/activities/MainActivity.java
@@ -2,13 +2,20 @@ package com.android.runweather.activities;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.runweather.R;
 import com.android.runweather.utils.LocationUtil;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.AutocompleteFilter;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 
 public class MainActivity extends AppCompatActivity {
+    TextView txtView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +25,39 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void getWeather(View view) {
-        String s = LocationUtil.getInstance(this).checkLocationPermission();
+        String locationServiceString = LocationUtil.getInstance(this).checkLocationPermission();
 
-        if (s.isEmpty()) {
-            //manually elicit location
+        if (locationServiceString.isEmpty()) {
+            getManualLocation();
         }
-
     }
+
+    private void getManualLocation() {
+        txtView = findViewById(R.id.txtView);
+
+        // Initialize the AutocompleteSupportFragment.
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+        AutocompleteFilter filter = new AutocompleteFilter.Builder()
+                .setTypeFilter(AutocompleteFilter.TYPE_FILTER_CITIES) //don't need anymore granularity than "City"
+                .build();
+        autocompleteFragment.setFilter(filter);
+
+        // Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                txtView.setText(place.getName());
+            }
+
+
+            @Override
+            public void onError(Status status) {
+                txtView.setText(status.toString());
+            }
+        });
+    }
+
+
 }
